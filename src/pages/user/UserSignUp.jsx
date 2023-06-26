3380485b852d2810b24e3fa839622422814694f3
DH-Pub <68387587+DH-Pub@users.noreply.github.com>
2023-06-26 10:35:29 +0700

    working sign up

--- src/pages/user/UserSignUp.jsx
+++ src/pages/user/UserSignUp.jsx
@@ -5,7 +5,7 @@ import { Box, Card, TextField, Toolbar } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import CustomLink from "../../components/CustomLink";
 import { createSelector } from "reselect";
-import { changeBio, changeEmail, changePassword, changePic, changeUsername, useSignUpMutation } from "../../store";
+import { changeBio, changeEmail, changePassword, changeUsername, useSignUpMutation } from "../../store";
 import { CustomTextArea } from "../../components/CustomTextArea";
 
 function UserSignUp() {
@@ -15,28 +15,23 @@ function UserSignUp() {
     (state) => state.signupForm.username,
     (state) => state.signupForm.password,
     (state) => state.signupForm.bio,
-    (state) => state.signupForm.pic,
-    (email, username, password, bio, pic) => ({ email, username, password, bio, pic })
+    (email, username, password, bio) => ({ email, username, password, bio })
   )
-  const { email, username, password, bio, pic } = useSelector(selectDetails);
+  const { email, username, password, bio } = useSelector(selectDetails);
 
   const emailRef = useRef();
   const errRef = useRef();
   const [errMsg, setErrMsg] = useState('');
   const [confirmPwd, setConfirmPwd] = useState('');
-  const [picUrl, setPicUrl] = useState('');
-  const [picName, setPicName] = useState('');
+  const [image, setImage] = useState(null);
   const navigate = useNavigate();
 
-  const [signup, { isLoading, error }] = useSignUpMutation();
+  const [signup, { isLoading }] = useSignUpMutation();
 
   useEffect(() => {
     emailRef.current.focus();
-  }, [])
-
-  useEffect(() => {
     setErrMsg('');
-  }, [email, username, password, bio, pic, confirmPwd])
+  }, [])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -48,46 +43,37 @@ function UserSignUp() {
       setErrMsg('Passwords do not match');
       return;
     }
-    const details = {
-      image: picUrl,
-      imageName: picName,
-      user: { email, username, password, bio },
-    }
+    const newUser = new FormData();
+
+    newUser.append("image", image);
+
+    const userJson = JSON.stringify({ email, username, password, bio });
+    const userBlob = new Blob([userJson], { type: "application/json" });
+    newUser.append("user", userBlob);
+
     try {
-      const result = await signup(details).unwrap();
+      const result = await signup(newUser).unwrap();
       console.log(result);
+      dispatch(changeEmail(''));
+      dispatch(changeUsername(''));
       dispatch(changePassword(''));
       setConfirmPwd('');
+      dispatch(changeBio(''));
+      navigate('/login');
     } catch (err) {
-      if (!err?.response) {
-        setErrMsg('No server Response');
-      } else if (err.response?.status === 400) {
-        setErrMsg('Missing Email or Password');
-      } else if (err.response?.status === 401) {
-        setErrMsg('Unauthorized');
-      } else {
-        setErrMsg('Login Failed');
-      }
       dispatch(changePassword(''));
       setConfirmPwd('');
-      errRef.current.focus();
+      setErrMsg(err.data);
+      // errRef.current.focus();
     }
   }
-
-  const toolbarHeight = 45;
 
   const handleUploadFile = e => {
     const file = e.target.files[0];
-    setPicName(file.name);
-    const reader = new FileReader();
-    reader.onload = () => {
-      const url = reader.result;
-      setPicUrl(url);
-      dispatch(changePic(url));
-    }
-    reader.readAsDataURL(file);
+    setImage(file);
   }
 
+  const toolbarHeight = 45;
   const content = (
     <Box sx={{
       minHeight: '100vh',
@@ -109,7 +95,9 @@ function UserSignUp() {
           width: 450, position: 'relative', top: toolbarHeight / -2
         }}>
           <h1 className='text-center font-extrabold text-2xl'>Create an account</h1>
+
           <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+
           <form onSubmit={handleSubmit}>
             <div className='p-4'>
               <TextField
--- src/store/apis/usersApi.js
+++ src/store/apis/usersApi.js
@@ -1,30 +1,13 @@
 import { authApi } from "./features/authApi";
-import { dataURItoBlob } from "./features/decodeBase64";
 
 export const usersApi = authApi.injectEndpoints({
   endpoints: (builder) => ({
     signUp: builder.mutation({
-      query: (details) => {
-        const formData = new FormData();
-
-        // add image
-        const type = details.image.split(",")[0].split(":")[1].split(";")[0];
-        const imageBlob = dataURItoBlob(details.image, type);
-
-        const imageFile = new File([imageBlob], details.imageName, {
-          type: type,
-        });
-        formData.append("image", imageFile);
-
-        const userJson = JSON.stringify(details.user);
-        const userBlob = new Blob([userJson], { type: "application/json" });
-        formData.append("user", userBlob);
-
-        console.log(formData);
+      query: (body) => {
         return {
           method: "POST",
           url: "/auth/signup",
-          body: formData,
+          body,
         };
       },
     }),
