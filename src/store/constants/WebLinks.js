c6a0f905dc39aec13d5e1e6d8fa0bb2aebf0957a
lavisar <sarlavi6969@gmail.com>
2023-07-06 21:52:36 +0700

    config redux API for UI

--- src/store/constants/WebLinks.js
+++ src/store/constants/WebLinks.js
@@ -1,30 +1,31 @@
 export const WEBLINKS = {
-  HOME: "/home",
-  LOGIN: "/login",
-  SIGNUP: "/sign-up",
-  PROFILE: "/profile",
+	HOME: "/home",
+	LOGIN: "/login",
+	SIGNUP: "/sign-up",
+	PROFILE: "/profile",
 
-  MAIN: "/",
-  TEAMS: "/teams",
+	MAIN: "/",
+	TEAMS: "/teams",
 
-  TASK: "/task",
+	TASK: "/task",
 
-  PROJECTS: "/projects",
+	PROJECTS: "/projects",
 
-  MILESTONE: "/milestone",
+	MILESTONE: "/milestone",
 
-  //
-  // admins links
-  ADMIN_MAIN: "/admin",
-  ADMIN_LOGIN: "/admin/login",
-  ADMIN_PROFILE: "/admin/profile",
-  ADMIN_ALL_USERS: "/admin/all-users",
-  ADMIN_MANAGE_ACCOUNT: "/admin/account",
-  ADMIN_CREATE: "/admin/create-admin",
+	DOCUMENT: "/document",
+	//
+	// admins links
+	ADMIN_MAIN: "/admin",
+	ADMIN_LOGIN: "/admin/login",
+	ADMIN_PROFILE: "/admin/profile",
+	ADMIN_ALL_USERS: "/admin/all-users",
+	ADMIN_MANAGE_ACCOUNT: "/admin/account",
+	ADMIN_CREATE: "/admin/create-admin",
 
-  ADMIN_ALL_TEAMS: "/admin/all-teams",
-  ADMIN_TEAM_DETAIL: "/admin/team",
+	ADMIN_ALL_TEAMS: "/admin/all-teams",
+	ADMIN_TEAM_DETAIL: "/admin/team",
 
-  ADMIN_ALL_PROJECTS: "/admin/all-projects",
-  ADMIN_PROJECT_DETAIL: "/admin/project",
+	ADMIN_ALL_PROJECTS: "/admin/all-projects",
+	ADMIN_PROJECT_DETAIL: "/admin/project",
 };
