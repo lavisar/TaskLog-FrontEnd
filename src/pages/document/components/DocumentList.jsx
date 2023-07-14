809ade3bb27c1cbfb0b9b1addc22e1f33e923b89
lavisar <sarlavi6969@gmail.com>
2023-07-14 17:21:27 +0700

    stage change

--- src/pages/document/components/DocumentList.jsx
+++ src/pages/document/components/DocumentList.jsx
@@ -1,13 +1,44 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { DataGrid } from "@mui/x-data-grid";
+import {
+	useDeleteDocumentByIdMutation,
+	useGetFileDownloadQuery,
+} from "../../../store/apis/documentApi";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { GetApp } from "@mui/icons-material";
 
 export const DocumentList = ({ documentLst }) => {
 	const dispatch = useDispatch();
 	const { data, isLoading, err } = documentLst;
-	const [inputValue, setInputValue] = useState("");
 	const [rows, setRows] = useState([]);
 	const [options, setOptions] = useState([""]);
+
+	const deleteDocumentById = useDeleteDocumentByIdMutation();
+	const getFileDownload = useGetFileDownloadQuery();
+
+	const handleDelete = async (documentId) => {
+		console.log("deleting id:", documentId);
+		try {
+			const result = await deleteDocumentById(documentId);
+			console.log(result);
+			if (result?.error.originalStatus === 200) {
+				return;
+			}
+			console.log("Document cannot be removed");
+		} catch (error) {
+			console.error("error", error);
+		}
+	};
+	const downloadFile = (documentId) => {
+		console.log("dowloading id: " + documentId);
+		getFileDownload(documentId).then((res) => {
+			const a = document.createElement("a");
+			a.href = URL.createObjectURL(res.data);
+			a.download = res.data.fileName || "file";
+			a.click();
+		});
+	};
 
 	useEffect(() => {
 		if (data) {
@@ -15,12 +46,21 @@ export const DocumentList = ({ documentLst }) => {
 				var inputString = row.files;
 				var indexOfUnderscore = inputString.indexOf("_");
 				var result = inputString.substring(indexOfUnderscore + 1);
+
+				const date = new Date(row.createAt);
+				const uploadedTime = date.toLocaleString("en-us", {
+					month: "short",
+					day: "numeric",
+					year: "numeric",
+					hour: "2-digit",
+					minute: "2-digit",
+				});
 				return {
 					id: index + 1,
 					documentId: row.id,
 					fileName: result,
 					fileDescription: row.description,
-					uploadedTime: row.createAt,
+					uploadedTime: uploadedTime,
 				};
 			});
 			setRows(rowConfig);
@@ -28,14 +68,29 @@ export const DocumentList = ({ documentLst }) => {
 	}, [data]);
 
 	const columns = [
-		{ field: "documentId", headerName: "Document ID", width: 150 },
+		{ field: "documentId", headerName: "Document ID", width: 200 },
 		{ field: "fileName", headerName: "File Name", width: 200 },
 		{
 			field: "fileDescription",
 			headerName: "File Description",
-			width: 250,
+			width: 400,
 		},
-		{ field: "uploadedTime", headerName: "Uploaded Time", width: 200 },
+		{ field: "uploadedTime", headerName: "Uploaded Time", width: 300 },
+		{
+			field: "actions",
+			headerName: "Actions",
+			width: 100,
+			renderCell: (params) => {
+				const documentId = params.row.documentId;
+				return (
+					<div className="flex items-center justify-center space-x-3">
+						<GetApp onClick={() => downloadFile(documentId)} />
+						<span className="mx-2">|</span>
+						<DeleteIcon onClick={() => handleDelete(documentId)} />
+					</div>
+				);
+			},
+		},
 	];
 
 	return (
